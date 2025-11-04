d05a014e3b970ff63ea4ca6d6ea0e6f70cbede6c
surajbkale <surajkalecsit@gmail.com>
2025-11-05 00:47:24 +0530

    feat: enhance game management with improved user handling and game state updates

--- apps/ws/src/GameManager.ts
+++ apps/ws/src/GameManager.ts
@@ -44,4 +44,168 @@ export class GameManager {
     this.users = this.users.filter((user) => user.socket !== socket);
     socketManager.removeUser(user);
   }
+
+  removeGame(gameId: string) {
+    this.games = this.games.filter((g) => g.gameId !== gameId);
+  }
+
+  private addHandler(user: User) {
+    user.socket.on("message", async (data) => {
+      const message = JSON.parse(data.toString());
+      if (message.type === INIT_GAME) {
+        if (this.pendingGameId) {
+          const game = this.games.find((x) => x.gameId === this.pendingGameId);
+          if (!game) {
+            console.error("Pending game not found");
+            return;
+          }
+          if (user.userId === game.player1UserId) {
+            socketManager.broadcast(
+              game.gameId,
+              JSON.stringify({
+                type: GAME_ALERT,
+                payload: {
+                  message: "Trying to Connect with yourself",
+                },
+              })
+            );
+            return;
+          }
+          socketManager.addUser(user, game.gameId);
+          await game?.updateSecondPlayer(user.userId);
+          this.pendingGameId = null;
+        } else {
+          const game = new Game(user.userId, null);
+          this.games.push(game);
+          this.pendingGameId = game.gameId;
+          socketManager.addUser(user, game.gameId);
+          socketManager.broadcast(
+            game.gameId,
+            JSON.stringify({
+              type: GAME_ADDED,
+              gameId: game.gameId,
+            })
+          );
+        }
+      }
+
+      if (message.type === MOVE) {
+        const gameId = message.payload.gameId;
+        const game = this.games.find((game) => game.gameId === gameId);
+        if (game) {
+          game.makeMove(user, message.payload.move);
+          if (game.result) {
+            this.removeGame(game.gameId);
+          }
+        }
+      }
+
+      if (message.type === EXIT_GAME) {
+        const gameId = message.payload.gameId;
+        const game = this.games.find((game) => game.gameId === gameId);
+
+        if (game) {
+          game.exitGame(user);
+          this.removeGame(game.gameId);
+        }
+      }
+
+      if (message.type === JOIN_GAME) {
+        const gameId = message.payload?.gameId;
+        if (!gameId) {
+          return;
+        }
+
+        let availableGame = this.games.find((game) => game.gameId === gameId);
+        const gameFromDb = await prisma.game.findUnique({
+          where: { id: gameId },
+          include: {
+            moves: {
+              orderBy: {
+                moveNumber: "asc",
+              },
+            },
+            blackPlayer: true,
+            whitePlayer: true,
+          },
+        });
+
+        // There is a game created but no second player available
+
+        if (availableGame && !availableGame.player2UserId) {
+          socketManager.addUser(user, availableGame.gameId);
+          await availableGame.updateSecondPlayer(user.userId);
+          return;
+        }
+
+        if (!gameFromDb) {
+          user.socket.send(
+            JSON.stringify({
+              type: GAME_NOT_FOUND,
+            })
+          );
+          return;
+        }
+
+        if (gameFromDb.status != GameStatus.IN_PROGRESS) {
+          user.socket.send(
+            JSON.stringify({
+              type: GAME_ENDED,
+              payload: {
+                result: gameFromDb.result,
+                status: gameFromDb.status,
+                moves: gameFromDb.moves,
+                blackPlayer: {
+                  id: gameFromDb.blackPlayer.id,
+                  name: gameFromDb.blackPlayer.name,
+                },
+                whitePlayer: {
+                  id: gameFromDb.whitePlayer.id,
+                  name: gameFromDb.whitePlayer.name,
+                },
+              },
+            })
+          );
+          return;
+        }
+
+        if (!availableGame) {
+          const game = new Game(
+            gameFromDb?.whitePlayer!,
+            gameFromDb?.blackPlayer!,
+            gameFromDb.id,
+            gameFromDb.startAt
+          );
+          game.seedMoves(gameFromDb?.moves || []);
+          this.games.push(game);
+          availableGame = game;
+        }
+
+        console.log(availableGame.getPlayer1TimeConsumed());
+        console.log(availableGame.getPlayer2TimeConsumed());
+
+        user.socket.send(
+          JSON.stringify({
+            type: GAME_JOINED,
+            payload: {
+              gameId,
+              moves: gameFromDb.moves,
+              blackPlayer: {
+                id: gameFromDb.blackPlayer.id,
+                name: gameFromDb.blackPlayer.name,
+              },
+              whitePlayer: {
+                id: gameFromDb.whitePlayer.id,
+                name: gameFromDb.whitePlayer.name,
+              },
+              player1TimeConsumed: availableGame.getPlayer1TimeConsumed(),
+              player2Timeconsumed: availableGame.getPlayer2TimeConsumed(),
+            },
+          })
+        );
+
+        socketManager.addUser(user, gameId);
+      }
+    });
+  }
 }
