d9cd670f68171f309f703c45703c508c5af6c9ec
surajbkale <surajkalecsit@gmail.com>
2025-11-08 21:45:29 +0530

    added the hooks and updated components

---
+++ apps/frontend/src/hooks/useThemes.ts
@@ -0,0 +1,8 @@
+import { useContext } from "react";
+import { ThemeContext } from "@/context/themeContext";
+import type { THEME_CONTEXT } from "@/context/themeContext";
+
+export function useThemeContext() {
+  const data = useContext(ThemeContext) as THEME_CONTEXT;
+  return data;
+}
