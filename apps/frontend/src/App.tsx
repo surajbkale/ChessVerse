9eab61ac47070dbfaf3359636d7bfa86d1ecf403
surajbkale <surajkalecsit@gmail.com>
2025-11-08 21:58:27 +0530

    refactor: restructure App component and add theming support

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,35 +1,68 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import "./App.css";
+import "./themes.css";
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Landing } from "./screens/Landing";
+import { Game } from "./screens/Game";
+import Login from "./screens/Login";
+import { Suspense } from "react";
+import { RecoilRoot } from "recoil";
+import { Loader } from "./components/Loader";
+import { Layout } from "./layout";
+import { Settings } from "./screens/Settings";
+import { Themes } from "./components/themes";
+import { ThemesProvider } from "./context/themeContext";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="min-h-screen bg-bgMain text-textMain">
+      <RecoilRoot>
+        <Suspense fallback={<Loader />}>
+          <ThemesProvider>
+            <AuthApp />
+          </ThemesProvider>
+        </Suspense>
+      </RecoilRoot>
+    </div>
+  );
 }
 
-export default App
+function AuthApp() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Layout>
+              <Landing />
+            </Layout>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/game/:gameId"
+          element={
+            <Layout>
+              <Game />
+            </Layout>
+          }
+        />
+        <Route
+          path="/settings"
+          element={
+            <Layout>
+              <Settings />
+            </Layout>
+          }
+        >
+          <Route path="themes" element={<Themes />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
