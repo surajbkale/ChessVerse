dd6b0b59516d52ce2fdc9022fbb9086382ba32c7
surajbkale <surajkalecsit@gmail.com>
2025-12-09 02:49:19 +0530

    fix: use relative paths for login

--- apps/frontend/src/screens/Login.tsx
+++ apps/frontend/src/screens/Login.tsx
@@ -3,8 +3,7 @@ import { useRef } from 'react';
 import { useRecoilState } from 'recoil';
 import { userAtom } from '@repo/store/userAtom';
 
-const BACKEND_URL =
-  import.meta.env.VITE_APP_BACKEND_URL ?? 'http://localhost:3000';
+const BACKEND_URL = (import.meta.env.VITE_APP_BACKEND_URL = '');
 
 const Login = () => {
   const navigate = useNavigate();
@@ -37,9 +36,7 @@ const Login = () => {
 
   return (
     <div className="flex flex-col items-center justify-center h-screen text-textMain">
-      <h1 className="text-4xl font-bold mb-8 text-center text-green-500 drop-shadow-lg">
-        Enter the Game World
-      </h1>
+      <h1 className="text-4xl font-bold mb-8 text-center text-green-500 drop-shadow-lg">Enter the Game World</h1>
       <div className="bg-bgAuxiliary2 rounded-lg shadow-lg p-8 flex flex-col md:flex-row">
         <div className="mb-8 md:mb-0 md:mr-8 justify-center flex flex-col">
           <div
